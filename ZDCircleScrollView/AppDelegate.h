a4e5cdc77651e81cedcb7fbc19306100afb9c46d
张海迪 <870347271@qq.com>
2014-12-06 16:47:33 +0800

    Initial Commit

---
+++ ZDCircleScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZDCircleScrollView
+//
+//  Created by 张海迪 on 14/12/6.
+//  Copyright (c) 2014年 haidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZDCircleScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZDCircleScrollView
+//
+//  Created by 张海迪 on 14/12/6.
+//  Copyright (c) 2014年 haidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
